d92fe4c160912dd29d81cefd42e7f8b214f34b72
Ben Chang <ben_chang@powerhouse.cloud>
2026-04-14 17:52:09 +0800

    feat(ui): add WebsiteActionButtons and shared types

---
+++ js/src/pages/AdminApp/Dashboard/SiteList/types.ts
@@ -0,0 +1,41 @@
+export interface IWebsite {
+	id: string
+	name: string
+	domain?: string
+	primaryDomain?: string
+	subDomain?: string
+	wildcardDomain: string
+	namespace: string
+	status: string
+	adminUsername: string
+	adminEmail: string
+	adminPassword: string
+	databaseName: string
+	databaseUsername: string
+	databasePassword: string | null
+	databaseRootPassword: string | null
+	package: {
+		id: string
+		name: string
+		description: string
+		price: string
+		wordpressSize: string
+		mysqlSize: string
+	} | null
+	user: {
+		id: string
+		firstName: string
+		lastName: string
+		email: string
+	} | null
+	phpPodSize: number
+	ipAddress: string
+	dailyCost?: number
+	createdAt: string
+	updatedAt: string
+}
+
+export interface IWebsiteResponse {
+	data: IWebsite[]
+	total: number
+}
